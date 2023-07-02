22dfe24ae53a12e61015b5af86555449d60ce978
GabrieleConte <gabryconte90@gmail.com>
2023-07-02 14:08:26 +0200

    minor fixes

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -61,7 +61,8 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEquals(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.get(email=payload['email']).exists()
+        user_exists = \
+            get_user_model().objects.get(email=payload['email']).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_user(self):
@@ -112,7 +113,12 @@ class PrivateUserApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = create_user(email='test@example.com',  password='testpass123',name='Test Name',)
+        self.user = \
+            create_user(
+                email='test@example.com',
+                password='testpass123',
+                name='Test Name',
+            )
         self.client.force_authenticate(user=self.user)
 
     def test_retrieve_profile_success(self):
